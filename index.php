1c7966eea67aa760f4f2b9910abfe18e928fb492
SamadhanNirali <samadhan.nirali@gmail.com>
2024-10-23 19:49:05 +0530

    aaa

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+function addition_of_two_numbers($a, $b)
+{
+
+    return $a + $b;
+}
+
+
+$variable_one = 12;
+$variable_two = 13;
+echo addition_of_two_numbers($variable_one, $variable_two);
---
+++ vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php
+
+return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '071e8c11672ec56f20cba227a466d2f11a3229de',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '071e8c11672ec56f20cba227a466d2f11a3229de',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'squizlabs/php_codesniffer' => array(
+            'pretty_version' => '3.10.3',
+            'version' => '3.10.3.0',
+            'reference' => '62d32998e820bddc40f99f8251958aed187a5c9c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../squizlabs/php_codesniffer',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+    ),
+);
